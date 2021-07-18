58fb8e55dd134fce7d32d3a932ad63c0636ffc6e
Surya Darma Putra <surya.igede@gmail.com>
2021-07-18 14:40:42 +0800

    test husky

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App(): JSX.Element {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React now
         </a>
       </header>
     </div>
